ced9e86c3a699ba8452ddc59520dccaed645668c
Andrés Gúrpide Lasheras <38692538+andresgur@users.noreply.github.com>
2025-04-09 02:41:34 +0100

    Removed unused import

--- fix_header.py
+++ fix_header.py
@@ -2,9 +2,8 @@
 # @Date:   04-16-2023
 # @Email:  a.gurpide-lasheras@soton.ac.uk
 # @Last modified by:   agurpide
-# @Last modified time: 04-16-2023
+# @Last modified time: 04-16-2024
 from astropy.io import fits
-import numpy as np
 import argparse
 import glob
 
@@ -12,9 +11,9 @@ ap = argparse.ArgumentParser(description='List of images for which the header ne
 ap.add_argument("input_images", nargs=1, help="Input images to be inspected", type=str)
 args = ap.parse_args()
 
-images = glob.glob(args.input_images)
+images = glob.glob(args.input_images[0])
 for i, image in enumerate(images):
-    print("Processing image: %s (%d/%d)" % (image, i, len(images)))
+    print("Processing image: %s (%d/%d)" % (image, i + 1, len(images)))
     imagefits = fits.open(image)
     date = imagefits[0].header["DATE-OBS"]
     y,m,d = date.split("-")
@@ -23,5 +22,6 @@ for i, image in enumerate(images):
     d = int(d)
     newymd = "%d-%02d-%02d" % (y,m,d)
     imagefits[0].header["DATE-OBS"] = newymd
-    imagefits[0].header["TIMEOBS"] = imagefits[0].header["EXP-STRT"]
-    fits.writeto(image.replace(".fits", "_fixed.fits"), overwrite=True)
+    imagefits[0].header['TIMEOBS'] = imagefits[0].header['EXP-STRT']
+    imagefits.writeto(image.replace('.fits','_fixed.fits'), overwrite=True)
+    imagefits.close()
